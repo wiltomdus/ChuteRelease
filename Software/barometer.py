27b3c40d9ca583fd8875e5e977f7f533593cc7ff
Dhenki <42845952+wiltomdus@users.noreply.github.com>
2024-10-13 15:02:04 -0400

    Fixed i2c device detection issues for barometer

--- Software/barometer.py
+++ Software/barometer.py
@@ -1,5 +1,7 @@
 """Barometer module"""
 
+import time
+import digitalio
 import board
 import busio
 from adafruit_dps310.advanced import DPS310_Advanced as DPS310
@@ -19,23 +21,46 @@ class Barometer(object):
     def init_barometer(self):
         """Initialize settings for the DPS310 sensor"""
         print("Initializing barometer...")
-        if self.i2c is None:
-            # Initialize the I2C bus if not already initialized
-            self.i2c = busio.I2C(board.SCL, board.SDA)
 
-        # Initialize the sensor only once
-        if self._sensor is None:
-            self._sensor = DPS310(self.i2c)
-            self._sensor.reset()
-            self._sensor.pressure_oversample_count = DPS310_SAMPLE_COUNT.COUNT_4
-            self._sensor.pressure_rate = DPS310_RATE.RATE_128_HZ
-            self._sensor.temperature_oversample_count = DPS310_SAMPLE_COUNT.COUNT_4
-            self._sensor.temperature_rate = DPS310_RATE.RATE_128_HZ
-            self._sensor.mode = DPS310_MODE.CONT_PRESTEMP
-            self._sensor.sea_level_pressure = 1024.6  # Set sea level pressure
-            self._sensor.wait_temperature_ready()
-            self._sensor.wait_pressure_ready()
-            print("Barometer initialization finished!")
+        # Set up the SDO pin
+        sdo = digitalio.DigitalInOut(board.SDO)
+        sdo.direction = digitalio.Direction.OUTPUT
+        sdo.value = True
+
+        # Initialize the I2C bus if not already initialized
+        i2c = busio.I2C(board.SCL, board.SDA)
+
+        # Scan for I2C devices
+        print("Scanning for I2C devices...")
+        while not i2c.try_lock():
+            pass
+        try:
+            devices = i2c.scan()
+            if not devices:
+                raise RuntimeError("No I2C devices found.")
+            print(
+                f"Found I2C devices at addresses: {[hex(device) for device in devices]}"
+            )
+            device = devices[0]
+            print(f"Selected I2C device at address: {hex(device)}")
+        finally:
+            i2c.unlock()
+
+        time.sleep(0.5)
+        # Set the sensor to high precision mode
+        self._sensor = DPS310(i2c, address=devices[0])
+        self._sensor.initialize()
+        self._sensor.pressure_oversample_count = DPS310_SAMPLE_COUNT.COUNT_4
+        self._sensor.pressure_rate = DPS310_RATE.RATE_128_HZ
+        self._sensor.temperature_oversample_count = DPS310_SAMPLE_COUNT.COUNT_4
+        self._sensor.temperature_rate = DPS310_RATE.RATE_128_HZ
+        self._sensor.mode = DPS310_MODE.CONT_PRESTEMP
+        self._sensor.sea_level_pressure = (
+            1013.25  #! Set local sea level pressure for more precision
+        )
+        self._sensor.wait_temperature_ready()
+        self._sensor.wait_pressure_ready()
+        print("Barometer initialization finished!")
 
     def get_sensor_data(self) -> tuple[float, float, float]:
         """Get current sensor data from barometer"""
